40b6fcd0e7533194fa05351f8b2f4cb796b4fb93
minwook <pmpmopm1@naver.com>
2019-06-21 10:39:14 +0900

    init commit

---
+++ app/src/main/java/com/minwook/mytriphistory/ui/MainActivity.java
@@ -0,0 +1,49 @@
+package com.minwook.mytriphistory.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.minwook.mytriphistory.R;
+import com.minwook.mytriphistory.adapter.MainTabAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        MainTabAdapter mainTabAdapter = new MainTabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mainTabAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.addOnPageChangeListener(new ViewPagerPageChangeListener());
+        //viewPager.addOnAdapterChangeListener();
+    }
+
+    class ViewPagerPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+}
